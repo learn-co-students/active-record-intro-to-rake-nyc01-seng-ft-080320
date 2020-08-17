187dd5a32b5659a6c23b957500317660ba68a353
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-16 23:14:36 -0400

    finished rake lab with personal study notes within

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 require 'pry'
 
-require_relative "../lib/student.rb"
+require_relative "../lib/student.rb" #student class is loaded unto this file
 
-DB = {:conn => SQLite3::Database.new("db/students.db")}
+DB = {:conn => SQLite3::Database.new("db/students.db")
+}#the database is loaded to this file, too
